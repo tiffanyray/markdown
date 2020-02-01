4b1861e3377b5ee277bc430673ea73fcec67e7bd
tiffanyray <tiffanyraylee@gmail.com>
2020-02-01 09:06:57 -0600

    Updates dependencies, refactored code, and ran build

--- src/app.js
+++ src/app.js
@@ -1,34 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './style.sass';
 
 let marked = require("marked");
 
-class Editor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            markdown: '# Welcome!\n## This is my Markdown Editor.\nErase me and type anything you would like!\n<a href=\"https://tiffanyray.github.io/portfolio\" target=\"_black\">Checkout My Portfolio</a>\n`<div> I promise I\'m cool :) </div>`\n```\nconst yourCoolToo = (name) => {\n  console.log(`${name}\'s cool too`);\n};\n```\n>Want more projects?\n\n Project | Link\n------------- | ------------- \nQuote Generator | <a href=\"https://tiffanyray.github.io/quoteGenerator/\" target=\"_black\">Get A Quote Now!</a> \nQuiz | <a href=\"https://tiffanyray.github.io/quiz/\" target=\"_black\">Should you go camping?</a>\nTicTacToe | <a href=\"https://tiffanyray.github.io/ticTacToe/\" target=\"_black\">Play TicTacToe Solo</a>'
-        }
-        this.handleChange = this.handleChange.bind(this);
+const Editor = () => {
+    let [markdown, setMarkdown] = useState('# Welcome!\n## This is my Markdown Editor.\nErase me and type anything you would like!\n<a href=\"https://tiffanyray.github.io/portfolio\" target=\"_black\">Checkout My Portfolio</a>\n`<div> I promise I\'m cool :) </div>`\n```\nconst yourCoolToo = (name) => {\n  console.log(`${name}\'s cool too`);\n};\n```\n>Want more projects?\n\n Project | Link\n------------- | ------------- \nQuote Generator | <a href=\"https://tiffanyray.github.io/quoteGenerator/\" target=\"_black\">Get A Quote Now!</a> \nQuiz | <a href=\"https://tiffanyray.github.io/quiz/\" target=\"_black\">Should you go camping?</a>\nTicTacToe | <a href=\"https://tiffanyray.github.io/ticTacToe/\" target=\"_black\">Play TicTacToe Solo</a>');
+
+    const handleChange = ({ target }) => {
+        setMarkdown(target.value)
     }
 
-    handleChange(e) {
-        const markdown = e.target.value;
-        this.setState ({ markdown });
-    }
-
-    render () {
-        let { markdown } = this.state;
-        return (
-            <div className="container">
-                <h1 className="title">Markdown Generator</h1>
-                <textarea value={markdown} onChange={(e) => this.handleChange(e)} id="editor" />
-                <div dangerouslySetInnerHTML={{__html: marked(markdown, {breaks: true})}} id="preview" ></div>
-            </div>
-        )
-    }
-
+    return <div className="container">
+        <h1 className="title">Markdown Generator</h1>
+        <textarea value={markdown} onChange={handleChange} id="editor" />
+        <div dangerouslySetInnerHTML={{__html: marked(markdown, {breaks: true})}} id="preview" />
+    </div>
 }
 
 ReactDOM.render(<Editor />, document.getElementById('root'));
